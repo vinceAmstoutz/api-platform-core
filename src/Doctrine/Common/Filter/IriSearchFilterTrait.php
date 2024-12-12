753776fa1d53f511ce1ef1901466026e582a718e
Vincent Amstoutz <vincent.amstoutz@les-tilleuls.coop>
2024-12-12 16:10:08 +0100

    feat(doctrine): search filters like laravel eloquent filters

---
+++ src/Doctrine/Common/Filter/IriSearchFilterTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Doctrine\Common\Filter;
+
+use ApiPlatform\Metadata\Parameter;
+use ApiPlatform\OpenApi\Model\Parameter as OpenApiParameter;
+
+trait IriSearchFilterTrait
+{
+    public function getOpenApiParameters(Parameter $parameter): OpenApiParameter|array|null
+    {
+        return new OpenApiParameter(name: $parameter->getKey().'[]', in: 'query', style: 'deepObject', explode: true);
+    }
+}
